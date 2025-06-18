37f9cd7c619572ce35a7d89d2cd5a4c3c80ec6a7
ahmedbenhcine <benhassine.ahmed@esprit.tn>
2025-06-18 22:04:55 +0100

    migrating to angular
    removing html file and changing everything to react to practice it more

---
+++ portfolio/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+ 
+const routes: Routes = [
+      { path: 'me', component: AboutmeComponent },
+        { path: 'project', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent }
+
+
+];
+
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ]
+})
+export class AppRoutingModule {}
---
+++ portfolio/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutmeComponent,
+    ProjectsComponent,
+    ContactComponent,
+    FooterComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    
+],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
